e2e011a3cd08f8a5c9d63ffc9e57e97dedcaf73f
Javier <javierotoalonso@gmail.com>
2025-06-08 17:07:34 +0200

    add publishing

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     id("dev.architectury.loom")
     id("architectury-plugin")
+    id("maven-publish")
+    id("signing")
 }
-
+group = "io.github.javivi09dev"
+version = "0.2.0"
 
 architectury {
     common("neoforge", "fabric")
@@ -13,6 +16,10 @@ loom {
     mixin {
         defaultRefmapName.set("pokemontoitem-common-refmap.json")
     }
+}
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 dependencies {
@@ -26,4 +33,56 @@ dependencies {
     modImplementation("org.apache.httpcomponents:httpclient:4.5.13")
     modImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     modImplementation("com.cobblemon:mod:${property("cobblemon_version")}") { isTransitive = false }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            
+            artifactId = "pokemontoitem"
+            
+            pom {
+                name.set("PokemonToItem")
+                description.set("Convertir Pokémon a items en Minecraft con Cobblemon")
+                url.set("https://github.com/javivi09dev/PokemonToItem")
+                
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("javivi09dev")
+                        name.set(project.findProperty("developer.name") as String? ?: "")
+                        email.set(project.findProperty("developer.email") as String? ?: "")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:git://github.com/javivi09dev/PokemonToItem.git")
+                    developerConnection.set("scm:git:ssh://github.com/javivi09dev/PokemonToItem.git")
+                    url.set("https://github.com/javivi09dev/PokemonToItem")
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "sonatype"
+            url = uri("https://central.sonatype.com/api/v1/publisher/deployments/upload/")
+            credentials {
+                username = project.findProperty("sonatype.username") as String? ?: ""
+                password = project.findProperty("sonatype.password") as String? ?: ""
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
